7759969726816a0a42575f0a43af63e6e26a1932
Lorna Timbah <me@webgrrrl.net>
2018-07-05 11:41:16 +0800

    Update wpomatic.php

--- wpomatic.php
+++ wpomatic.php
@@ -3,8 +3,8 @@
 Plugin Name: WP-o-Matic
 Plugin URI: http://themeskult.com/wp-o-matic/
 Description: Automated posts via RSS feed aggregation.
-Version: 2.4.0
-Author: Themes Kult
+Version: 2.4.1
+Author: Themes Kult, webgrrrl
 Author URI: http://themeskult.com/
 */
 
